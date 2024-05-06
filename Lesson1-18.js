5b986d76da430be257b1a5d09af5fd6365200d27
Ap89kg <ap89kg@gmail.com>
2024-05-06 11:45:58 +0300

    Lesson 19

---
+++ Lesson1-18.js
@@ -0,0 +1,21 @@
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+/* alert('Hello'); */
+
+// const result = confirm("Are you тута?")
+// console.log(result);
+
+// const answer = prompt("18 есть???????", "01")
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt ('what your name?', '');
+answers[1] = prompt ('what your ?', '');
+answers[2] = prompt ('what?', '');
+
+document.write(answers);
